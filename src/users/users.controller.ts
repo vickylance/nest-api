28a250e4586e3579123f8855f19d799e01040e70
Vignesh S <vignesh6993@live.in>
2024-03-16 00:06:22 +0530

    adds ParseIntPipe for id params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
@@ -28,8 +29,8 @@ export class UsersController {
   }
 
   @Get(':id') // GET /users/:id
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id); // + unary will convert string to number
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id); // + unary will convert string to number
   }
 
   @Post() // POST /users
@@ -46,7 +47,7 @@ export class UsersController {
 
   @Patch(':id') // PATCH /users/:id
   update(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body()
     userUpdate: {
       name?: string;
@@ -54,11 +55,11 @@ export class UsersController {
       role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
     },
   ) {
-    return this.usersService.update(+id, userUpdate);
+    return this.usersService.update(id, userUpdate);
   }
 
   @Delete(':id') // DELETE /users/:id
-  remove(@Param('id') id: string) {
-    return this.usersService.delete(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }

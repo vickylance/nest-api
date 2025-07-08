e479cf7478d93530ef3469faa44c23294736fdbd
Vignesh S <vignesh6993@live.in>
2025-07-08 13:50:09 +0530

    added devtools integration and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
